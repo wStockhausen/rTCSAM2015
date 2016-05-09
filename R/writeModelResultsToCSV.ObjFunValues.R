b2914c5ab6eb016387c32375d71c64d00569bebd
wStockhausen <william.stockhausen@noaa.gov>
2016-05-09 06:41:44 -0800

    Added writeModelResultsToCSV.ObjFunValues.

--- R/writeModelResultsToCSV.ObjFunValues.R
+++ R/writeModelResultsToCSV.ObjFunValues.R
@@ -1,31 +1,38 @@
 #'
-#'@title Plot all objective function values
+#'@title Write all objective function values to csvs
 #'
-#'@description Function to plot all objective function values.
+#'@description Function to write all objective function values to csvs.
 #'
 #'@param repObjs - model report object or list of report objects
-#'@param variable - name of variable to plot
-#'@param csv - csf file name to export dataframe to
+#'@param csv - base filename for csvs to export dataframes to
 #'@param verbose - flag (T/F) to print diagnostic info
 #'
-#'@return dataframe with objective function components for penalties, priors, and data.
+#'@return list w two dataframes with objective function components, 
+#'one for penalties and priors (...PP.csv) and one for data (...Data.csv).
 #'
 #'@export
 #'
 writeModelResultsToCSV.ObjFunValues<-function(repObjs,
-                                              variable='objfun',
-                                              csv="ModelComparisons.ObjFunValues.csv",
+                                              csv="ModelComparisons.ObjFunValues",
                                               verbose=FALSE){
     if (verbose) cat("plotting objective function values for penalties.\n")
-    mdfr<-getObjFunValues.Penalties(repObjs,verbose=verbose);
+    mdfr1<-getObjFunValues.Penalties(repObjs,verbose=verbose);
 
     if (verbose) cat("plotting objective function values for priors.\n")
-    mdfr<-getObjFunValues.Priors(repObjs,verbose=verbose);
+    mdfr2<-getObjFunValues.Priors(repObjs,verbose=verbose);
+    
+    mdfr<-rbind(mdfr1,mdfr2);
+    dfr<-reshape2::dcast(mdfr,"type+category+name+level~variable+model",value.var="value")
+    write.csv(dfr,file=paste0(csv,".PP.csv"),row.names=FALSE)
 
     if (verbose) cat("plotting objective function values for data.\n")
-    mdfr<-getObjFunValues.Data(repObjs,verbose=verbose);
+    mdfr3<-getObjFunValues.Data(repObjs,verbose=verbose);
+    dfr3a<-reshape2::dcast(mdfr3,
+                           "source.type+source.name+catch.type+data.type+fit.type+nll.type+sex+maturity+shell_condition~variable+model",
+                           fun.aggregate=sum,
+                           value.var="value")
+    write.csv(dfr,file=paste0(csv,".Data.csv"),row.names=FALSE)
     
-    return(plots);
+    return(list(pp=dfr,data=dfr3a));
 }
 
-#plots<-compareModelResults.ObjFunValues(repObjs); print(plots);
