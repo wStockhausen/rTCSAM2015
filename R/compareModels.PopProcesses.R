8fe6effb7aabdee643b9890463d830eef2194df6
wStockhausen <william.stockhausen@noaa.gov>
2016-06-13 20:39:18 -0700

    Revised compareModels.PopProcesses() to use getMDFR... functions.

--- R/compareModels.PopProcesses.R
+++ R/compareModels.PopProcesses.R
@@ -1,5 +1,5 @@
 #'
-#'@title Compare population process-related quantities from TCSAM2015 and rsimTCSAM model runs.
+#'@title Compare population process-related quantities from TCSAM2015 and rsimTCSAM model runs
 #'
 #'@description Function to compare population process-related quantities from TCSAM2015 and rsimTCSAM model runs.
 #'
@@ -47,6 +47,16 @@ compareModels.PopProcesses<-function(tcsams=NULL,
     if (showPlot||!is.null(pdf)) print(p);
     plots$M_cxm<-p;
     
+    #molt-to-maturity
+    if (verbose) cat("Plotting molt to maturity info\n");
+    mdfr<-getMDFR.prM2M(tcsams,rsims,verbose);
+    p<-plotMDFR.XY(mdfr,x='z',agg.formula='pc+x+model+z',faceting='pc~x',
+                   colour='model',guideTitleColour='',
+                   shape='model',guideTitleShape='',
+                   xlab='size (mm CW)',ylab='pr(molt-to-maturity)');
+    if (showPlot||!is.null(pdf)) print(p);
+    plots$prM2M_cz<-p;
+        
     #mean growth increments
     if (verbose) cat("Plotting mean growth increments\n");
     mdfr<-getMDFR.MeanGrowthIncrements(tcsams,rsims,verbose);
@@ -68,73 +78,9 @@ compareModels.PopProcesses<-function(tcsams=NULL,
     if (showPlot||!is.null(pdf)) print(p);
     plots$T_czz<-p;
     
-    #molt-to-maturity
-    if (verbose) cat("Plotting molt to maturity info\n");
-    mdfr<-NULL;
-    if (!is.null(tcsams)){
-        mdfr<-getMDFR('mp/prM2M_cz',tcsams,NULL);
-        mdfr$y<-'';
-        mdfr$x<-'';
-        ums<-as.character(unique(mdfr$model))
-        for (um in ums){
-            tcsam<-tcsams[[um]];
-            pgi<-tcsam$mpi$mat$pgi;
-            nPCs<-length(pgi$pcs)-1;#last element is a NULL
-            for (pc in 1:nPCs){
-                idx<-(mdfr$pc==pc)&(mdfr$model==um);
-                mdfr$y[idx]<-pgi$pcs[[pc]]$YEAR_BLOCK;
-                mdfr$x[idx]<-tolower(pgi$pcs[[pc]]$SEX);
-                mdfr$y[idx]<-reformatTimeBlocks(mdfr$y[idx],tcsam$mc$dims);
-            }
-        }
-        mdfr$pc<-mdfr$y;
-        mdfr<-mdfr[,c('pc','x','z','val','model','modeltype')];
-    }
-    if (!is.null(rsims)){
-        mdfrp<-getMDFR('mp/prM2M_cxz',NULL,rsims);
-        ums<-as.character(unique(mdfrp$model))
-        for (um in ums){
-            idx<-(mdfrp$model==um);
-            mdfrp$pc[idx]<-reformatTimeBlocks(mdfrp$pc[idx],rsims[[um]]$mc$dims);
-        }
-        mdfr<-rbind(mdfr,mdfrp);
-    }
-    p<-plotMDFR.XY(mdfr,x='z',agg.formula='pc+x+model+z',faceting='pc~x',
-                   colour='model',guideTitleColour='',
-                   shape='model',guideTitleShape='',
-                   xlab='size (mm CW)',ylab='pr(molt-to-maturity)');
-    if (showPlot||!is.null(pdf)) print(p);
-    plots$prM2M_cz<-p;
-        
     #recruitment size distribution
     if (verbose) cat("Plotting recruitment size distribution\n");
-    path<-'mp/R_list/R_cz';
-    if (!is.null(tcsams)){
-        mdfr<-getMDFR(path,tcsams,NULL);
-        mdfr$y<-'';
-        ums<-as.character(unique(mdfr$model))
-        for (um in ums){
-            tcsam<-tcsams[[um]];
-            pgi<-tcsam$mpi$rec$pgi;
-            nPCs<-length(pgi$pcs)-1;#last element is a NULL
-            for (pc in 1:nPCs){
-                idx<-(mdfr$pc==pc)&(mdfr$model==um);
-                mdfr$y[idx]<-pgi$pcs[[pc]]$YEAR_BLOCK;
-                mdfr$y[idx]<-reformatTimeBlocks(mdfr$y[idx],tcsam$mc$dims);
-            }
-        }
-        mdfr$pc<-mdfr$y;
-        mdfr<-mdfr[,c('pc','z','val','model','modeltype')];
-    }
-    if (!is.null(rsims)){
-        mdfrp<-getMDFR(path,NULL,rsims);
-        ums<-as.character(unique(mdfrp$model))
-        for (um in ums){
-            idx<-(mdfrp$model==um);
-            mdfrp$pc[idx]<-reformatTimeBlocks(mdfrp$pc[idx],rsims[[um]]$mc$dims);
-        }
-        mdfr<-rbind(mdfr,mdfrp);
-    }
+    mdfr<-getMDFR.RecSizeDistribution(tcsams,rsims,verbose);
     p<-plotMDFR.XY(mdfr,x='z',agg.formula='pc+x+model+z',faceting='pc~.',
                    xlab='size (mm CW)',ylab='Recruitment size distribution',units='millions',lnscale=FALSE,
                    colour='model',guideTitleColour='',
@@ -145,32 +91,7 @@ compareModels.PopProcesses<-function(tcsams=NULL,
     #recruitment sex ratio
     if (verbose) cat("Plotting recruitment sex ratio\n");
     path<-'mp/R_list/Rx_c';
-    if (!is.null(tcsam)){
-        mdfr<-getMDFR(path,tcsams,NULL);
-        mdfr$y<-'';
-        ums<-as.character(unique(mdfr$model))
-        for (um in ums){
-            tcsam<-tcsams[[um]];
-            pgi<-tcsam$mpi$rec$pgi;
-            nPCs<-length(pgi$pcs)-1;#last element is a NULL
-            for (pc in 1:nPCs){
-                idx<-(mdfr$pc==pc)&(mdfr$model==um);
-                mdfr$y[idx]<-pgi$pcs[[pc]]$YEAR_BLOCK;
-                mdfr$y[idx]<-reformatTimeBlocks(mdfr$y[idx],tcsam$mc$dims);
-            }
-        }
-        mdfr$pc<-mdfr$y;
-        mdfr<-mdfr[,c('pc','val','model','modeltype')];
-    }
-    if (!is.null(rsims)){
-        mdfrp<-getMDFR(path,NULL,rsims);
-        ums<-as.character(unique(mdfrp$model))
-        for (um in ums){
-            idx<-(mdfrp$model==um);
-            mdfrp$pc[idx]<-reformatTimeBlocks(mdfrp$pc[idx],rsims[[um]]$mc$dims);
-        }
-        mdfr<-rbind(mdfr,mdfrp);
-    }
+    mdfr<-getMDFR.SexRatio(tcsams,rsims,verbose);
     p<-plotMDFR.Bars(mdfr,x='pc',agg.formula=NULL,faceting=NULL,
                      fill='model',xlab='year block',ylab='sex ratio');
     if (showPlot||!is.null(pdf)) print(p);
